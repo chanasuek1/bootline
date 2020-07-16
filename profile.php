8217e6b7a103d23d6eaa29352e435342d6b75416
chanasuek1 <47407518+chanasuek1@users.noreply.github.com>
2020-07-16 20:15:50 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '3PZEAbBRsdLiIs3iUG7SuaOQfzV6GmcuhGgA/IeR5H1BSUPmORc9RZCu/wVeT9i27hxtBAjg5ZW5yrFWcFe7C0mwOWL5QqFKdpLeuwdR/DsnzFqn+NpFonQRNMm+pcZ0USoLUSHLnHZZ/v12xn0X2wdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'U0e535834a2eabf1a9df3bd7be4fc94f5';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
