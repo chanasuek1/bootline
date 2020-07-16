3a92402003dead9432690e5fbc52428b6af8a70c
chanasuek1 <47407518+chanasuek1@users.noreply.github.com>
2020-07-16 20:06:07 +0700

    Update send.php

--- send.php
+++ send.php
@@ -1,8 +1,8 @@
 <?php
 require "vendor/autoload.php";
-$access_token = 'xxxxx';
-$channelSecret = 'xxxxx';
-$idPush = 'xxxxxx'
+$access_token = '3PZEAbBRsdLiIs3iUG7SuaOQfzV6GmcuhGgA/IeR5H1BSUPmORc9RZCu/wVeT9i27hxtBAjg5ZW5yrFWcFe7C0mwOWL5QqFKdpLeuwdR/DsnzFqn+NpFonQRNMm+pcZ0USoLUSHLnHZZ/v12xn0X2wdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '4f96e6c9e3b02fd6a2a8f3808f2a9042';
+$idPush = 'U0e535834a2eabf1a9df3bd7be4fc94f5'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
